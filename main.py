65c5f992fb630a9270d91fd822c028d0e41f8c1f
adexcell <46703600+adexcell@users.noreply.github.com>
2019-02-14 12:34:29 +0500

    Update main.py

--- main.py
+++ main.py
@@ -1,28 +1,28 @@
 import requests
 
-API_KEY = 'trnsl.1.1.20161025T233221Z.47834a66fd7895d0.a95fd4bfde5c1794fa433453956bd261eae80152'
-URL = 'https://translate.yandex.net/api/v1.5/tr.json/translate'
-
 def translate_it(source_path, result_path, from_lang, to_lang='ru'):
-    print('Start translation -', source_path)
-    def translate_f():
-        with open(source_path, encoding='utf-8') as source_f:
-            text = source_f.read()
+  API_KEY = 'trnsl.1.1.20161025T233221Z.47834a66fd7895d0.a95fd4bfde5c1794fa433453956bd261eae80152'
+  URL = 'https://translate.yandex.net/api/v1.5/tr.json/translate'
+  print('Start translation -', source_path)
+    
+  with open(source_path, encoding='utf-8') as source_f:
+    text = source_f.read()
 
-            params = {
-                'key': API_KEY,
-                'text': text,
-                'lang': '{}-{}'.format(from_lang, to_lang),
-            }
+  params = {
+    'key': API_KEY,
+    'text': text,
+    'lang': '{}-{}'.format(from_lang, to_lang),
+  }
 
-            response = requests.get(URL, params=params)
-            json_ = response.json()
-            return ''.join(json_['text'])
+  response = requests.get(URL, params=params)
+  json_ = response.json()
+  output = ''.join(json_['text'])
 
-    with open(result_path, 'w', encoding='utf-8') as result_f:
-        result_f.write(translate_f())
-    print('End -', result_path)
-    print()
+  with open(result_path, 'w', encoding='utf-8') as result_f:
+   result_f.write(output)
+
+  print('End -', result_path)
+  print()
 
 translate_it('DE.txt', 'Translate_DE.txt', 'de')
 translate_it('ES.txt', 'Translate_ES.txt', 'es')
